52060e8a92eff66c30018c039c31d300cd28272d
JD037 <joj237@gmail.com>
2023-04-22 13:31:37 -0700

    2.task 1st commit

--- binary_trees.h
+++ binary_trees.h
@@ -29,4 +29,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* Function to insert a node as the left-child of another node */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
+/* Function to insert a node as the right-child of another node */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 #endif /* _BINARY_TREES_H_ */
